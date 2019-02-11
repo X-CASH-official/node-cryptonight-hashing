514839edb00285ee4715b4da3363520a3b92e7cb
X-CASH-official <41022492+X-CASH-official@users.noreply.github.com>
2019-02-11 07:51:38 -0800

    Updated Iterations count
    Updated Iterations count

--- xmrig/crypto/CryptoNight_constants.h
+++ xmrig/crypto/CryptoNight_constants.h
@@ -40,7 +40,7 @@ namespace xmrig
 constexpr const size_t   CRYPTONIGHT_MEMORY       = 2 * 1024 * 1024;
 constexpr const uint32_t CRYPTONIGHT_MASK         = 0x1FFFF0;
 constexpr const uint32_t CRYPTONIGHT_ITER         = 0x80000;
-constexpr const uint32_t CRYPTONIGHT_HALF_ITER    = 0x40000;
+constexpr const uint32_t CRYPTONIGHT_HALF_ITER    = 0x100000;
 constexpr const uint32_t CRYPTONIGHT_XAO_ITER     = 0x100000;
 
 constexpr const uint32_t CRYPTONIGHT_GPU_ITER     = 0xC000;
